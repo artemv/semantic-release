92328645c64046ddd14e2531b79109216b96e927
Artem Vasiliev <artem.job@gmail.com>
2017-09-14 01:49:16 +0300

    chore(ci): wait for couchdb first

---
+++ test/registry/wait-for-couch.sh
@@ -0,0 +1,15 @@
+attempt=0
+success=false
+while [ $attempt -le 590 ]; do
+    attempt=$(( $attempt + 1 ))
+    echo "Waiting for server to be up (attempt: $attempt)..."
+    curl http://127.0.0.1:5984 > couch-status.json
+    rc=$?
+    if [[ $rc = 0 ]] ; then
+      echo "CouchDB is up!"
+      cat couch-status.json
+      exit 0
+    fi
+    sleep 2
+done
+exit 1
