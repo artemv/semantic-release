b0918700f4f9c20a5e9696775f24e68cec8a69db
Artem Vasiliev <artem.job@gmail.com>
2017-09-14 01:25:02 +0300

    chore(ci): couch config for CircleCI
    pretty weird but it works this way

--- test/registry/start.sh
+++ test/registry/start.sh
@@ -9,21 +9,20 @@ mkdir -p couch
 
 if [[ $TRAVIS = true ]]
 then
-  COUCH=http://admin:password@127.0.0.1:5984
-
-  curl -X PUT http://127.0.0.1:5984/_config/admins/admin -d '"password"'
-
-  curl -X PUT $COUCH/_config/couchdb/delayed_commits -d '"false"'
-  curl -X PUT $COUCH/_config/couch_httpd_auth/users_db_public -d '"true"'
-  curl -X PUT $COUCH/_config/couch_httpd_auth/public_fields -d '"appdotnet, avatar, avatarMedium, avatarLarge, date, email, fields, freenode, fullname, github, homepage, name, roles, twitter, type, _id, _rev"'
-  curl -X PUT $COUCH/_config/httpd/secure_rewrites -d '"false"'
-
 else
-  COUCH=http://admin:password@127.0.0.1:15986
+  mkdir couch
   couchdb -b -a local.ini -p couch/pid -o couch/stdout.log -e couch/stderr.log
   # wait for couch to start
   sleep 1
 fi
+COUCH=http://admin:password@127.0.0.1:5984
+
+curl -X PUT http://127.0.0.1:5984/_config/admins/admin -d '"password"'
+
+curl -X PUT $COUCH/_config/couchdb/delayed_commits -d '"false"'
+curl -X PUT $COUCH/_config/couch_httpd_auth/users_db_public -d '"true"'
+curl -X PUT $COUCH/_config/couch_httpd_auth/public_fields -d '"appdotnet, avatar, avatarMedium, avatarLarge, date, email, fields, freenode, fullname, github, homepage, name, roles, twitter, type, _id, _rev"'
+curl -X PUT $COUCH/_config/httpd/secure_rewrites -d '"false"'
 
 # create "registry" database
 curl -X PUT $COUCH/registry
